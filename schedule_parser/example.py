8ea9bf29ce723c117d128331fc36b7260f4764e1
shrechochek <stephan.martynov41@gmail.com>
2026-01-19 22:20:21 +0300

    add config.py

--- schedule_parser/example.py
+++ schedule_parser/example.py
@@ -1,10 +1,5 @@
 import schedule_parser as sp
-
-url = (
-    "https://docs.google.com/spreadsheets/d/" # гугл таблицы
-    "1W9qMX1QzlZvBkNS0lwA7ZKyMGUlR9dBZnAE9JwqHRHg/export" # id таблицы
-    "?format=csv&gid=917584427" # id страницы
-)
+from config import *
 
 strucutred_table = sp.init_structured_table(url=url)
 
