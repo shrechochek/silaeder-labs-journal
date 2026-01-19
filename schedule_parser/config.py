e00f669bb79ffad70247d741121fb4a181c7be7b
shrechochek <stephan.martynov41@gmail.com>
2026-01-20 00:34:48 +0300

    improve parsing and return .json file instead of array

--- schedule_parser/config.py
+++ schedule_parser/config.py
@@ -3,3 +3,8 @@ url = (
     "1W9qMX1QzlZvBkNS0lwA7ZKyMGUlR9dBZnAE9JwqHRHg/export" # id таблицы
     "?format=csv&gid=917584427" # id страницы
 )
+
+classes_start_marker = "5С"
+classes_stop_marker = "11С"
+days_of_week_start_marker = "Понедельник"
+days_of_week_stop_marker = "Cуббота"
